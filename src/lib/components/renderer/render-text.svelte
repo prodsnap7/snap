8204d9529ef3c0b922e8b909b7042bf39e405f7a
Faadhil Hakim <faadhil@nexttrack.biz>
2023-12-30 18:21:07 -0600

    some text enhancements!

--- src/lib/components/renderer/render-text.svelte
+++ src/lib/components/renderer/render-text.svelte
@@ -8,12 +8,25 @@
 	};
 
 	const { text, hide = false, scale = 1 } = $props<Props>();
+	let textRef = $state<HTMLDivElement>();
 
-	$inspect(text.scale);
+	$effect(() => {
+		text.lineHeight;
+		text.fontSize;
+		text.content;
+		text.fontStyle;
+		text.fontWeight;
+		text.fontFamily;
+		text.letterSpacing;
+		text.uppercase;
+
+		text.height = textRef?.clientHeight || text.height;
+	});
 </script>
 
 {#if !hide}
 	<div
+		bind:this={textRef}
 		class="text-renderer overflow-hidden whitespace-pre-wrap cursor-pointer outline-none user-select-none"
 		style="
     font-size: {text.fontSize * text.scale}px;
@@ -24,7 +37,7 @@
     text-decoration: {text.decoration};
     text-align: {text.align};
     width: {text.width}px;
-		height: {text.height}px;
+		height: auto;
     letter-spacing: {text.letterSpacing}px;
     line-height: {text.lineHeight * text.fontSize * text.scale}px;
 		text-transform: {text.uppercase ? 'uppercase' : 'none'};

a9590458b38592368b3e3d4c03fd12b43784cfc6
Faadhil Hakim <faadhil@nexttrack.biz>
2023-12-31 23:16:06 -0600

    add crop icon

--- src/lib/components/toolbar/image.svelte
+++ src/lib/components/toolbar/image.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { canvasStore } from '$lib/store';
+	import { Crop } from 'phosphor-svelte';
 	import Button from '../ui/button/button.svelte';
+	import clsx from 'clsx';
 
 	function onCropClick() {
 		if (canvasStore.state === 'cropping') {
@@ -11,4 +13,12 @@
 	}
 </script>
 
-<Button variant="outline" onclick={onCropClick}>Crop</Button>
+<Button
+	class={clsx('p-1 cursor-pointer border border-transparent', {
+		'border-primary': canvasStore.state === 'cropping'
+	})}
+	variant={canvasStore.state === 'cropping' ? 'secondary' : 'ghost'}
+	onclick={onCropClick}
+>
+	<Crop size={24} />
+</Button>

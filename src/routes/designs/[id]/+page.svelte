3bd39cf901fbd28c6e55624a2c1175be4a9400a9
Faadhil Hakim <faadhil@nexttrack.biz>
2024-01-02 16:54:27 -0600

    add avator and finish header

--- src/routes/designs/[id]/+page.svelte
+++ src/routes/designs/[id]/+page.svelte
@@ -10,8 +10,19 @@
 	import Logo from '$lib/components/logo-mini.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Menubar from '$lib/components/ui/menubar';
+	import * as Popover from '$lib/components/ui/popover';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
-	import { ArrowUUpLeft, ArrowUUpRight, Folder } from 'phosphor-svelte';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import {
+		ArrowUUpLeft,
+		ArrowUUpRight,
+		Check,
+		Download,
+		FilePng,
+		Folder,
+		X
+	} from 'phosphor-svelte';
+	import Slider from '$lib/components/ui/slider/slider.svelte';
 
 	export let data: PageData;
 	store.init(data.design);
@@ -57,6 +68,36 @@
 				</Menubar.Menu>
 			</Menubar.Root>
 
+			<Popover.Root>
+				<Popover.Trigger>
+					<Button variant="ghost" class="text-sm">Resize</Button>
+				</Popover.Trigger>
+
+				<Popover.Content>
+					<div class="mb-4 text-sm font-semibold">Custom Size</div>
+					<div class="flex text-xs items-center gap-2">
+						<div
+							class="w-20 h-8 flex rounded-sm border items-center p-1 gap-1 focus-within:ring focus-within:ring-primary"
+						>
+							<span class="text-sm text-muted-foreground">W</span>
+							<input
+								class="w-full h-full ml-2 oultine-none text-xs focus:outline-none bg-background"
+							/>
+						</div>
+						<div
+							class="w-20 h-8 flex rounded-sm border items-center p-1 gap-1 focus-within:ring focus-within:ring-primary"
+						>
+							<span class="text-sm text-muted-foreground">H</span>
+							<input
+								class="w-full h-full ml-2 oultine-none text-xs focus:outline-none bg-background"
+							/>
+						</div>
+
+						<div class="p-1.5 h-8 border pointer-events-none text-center">px</div>
+					</div>
+				</Popover.Content>
+			</Popover.Root>
+
 			<div class="flex items-center gap-2 w-28">
 				<Separator class="h-6" orientation="vertical" />
 				<Button variant="ghost">
@@ -67,8 +108,49 @@
 				</Button>
 				<Separator class="h-6" orientation="vertical" />
 			</div>
-			<div class="mr-4 md:flex">
-				<Input class="px-1 text-sm h-8" value={store.name} />
+
+			<div class="flex-1" />
+			<div class="mr-2 flex items-center gap-2">
+				<Input class="px-1 text-sm h-8" bind:value={store.name} />
+
+				<Popover.Root>
+					<Popover.Trigger>
+						<Button class="font-semibold">
+							<Download class="mr-2" size={20} />
+							Download
+						</Button>
+					</Popover.Trigger>
+					<Popover.Content>
+						<div class="font-semibold my-2">Download Your Design</div>
+						<Input class="w-full my-4" bind:value={store.name} />
+						<div class="p-2 bg-secondary/50 rounded-sm">
+							<div class="flex items-center justify-between">
+								<span class="flex text-xs font-bold">
+									<FilePng class="mr-1" size={20} />
+									PNG
+								</span>
+								<Check size={20} />
+							</div>
+							<p class="text-sm text-muted-foreground my-2">
+								To keep high quality in complex images.
+							</p>
+							<div class="flex items-center gap-1">
+								<Slider step={0.25} min={0.25} max={4} value={[1]} />
+								1x
+							</div>
+						</div>
+
+						<Button class="w-full mt-4">
+							<Download class="mr-2" size={20} />
+							Download
+						</Button>
+					</Popover.Content>
+				</Popover.Root>
+
+				<Avatar.Root>
+					<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+					<Avatar.Fallback>CN</Avatar.Fallback>
+				</Avatar.Root>
 			</div>
 		</div>
 	</header>

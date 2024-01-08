90a9404e22389acb3c611e8f33877fa869d9c3e0
Faadhil Hakim <faadhil@nexttrack.biz>
2024-01-08 13:07:06 -0600

    update new design button

--- src/routes/designs/+page.svelte
+++ src/routes/designs/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import { Checkbox } from '$lib/components/ui/checkbox';
+	import Label from '$lib/components/ui/label/label.svelte';
 	import Logo from '$lib/components/logo.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import { File, Folder } from 'phosphor-svelte';
+	import { Check, File, Folder, X } from 'phosphor-svelte';
 	import * as Avatar from '$lib/components/ui/avatar';
+	import * as Popover from '$lib/components/ui/popover';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+
+	let width = $state(2000);
+	let height = $state(2000);
 </script>
 
 <div class="h-screen">
@@ -10,10 +17,105 @@
 		<Logo />
 		<div class="flex-1"></div>
 
-		<Button>
-			<File size={20} class="mr-2" />
-			New Design
-		</Button>
+		<Popover.Root>
+			<Popover.Trigger>
+				<Button>
+					<File size={20} class="mr-2" />
+					New Design
+				</Button>
+			</Popover.Trigger>
+
+			<Popover.Content>
+				<div class="mb-4 text-sm font-semibold">Custom Size</div>
+				<div class="flex text-xs items-center gap-2">
+					<div
+						class="w-20 h-8 flex rounded-sm border items-center p-1 gap-1 focus-within:border-primary"
+					>
+						<span class="text-sm text-muted-foreground">W</span>
+						<input
+							type="number"
+							bind:value={width}
+							class="w-full h-full ml-2 oultine-none text-xs focus:outline-none bg-background"
+						/>
+					</div>
+					<div
+						class="w-20 h-8 flex rounded-sm border items-center p-1 gap-1 focus-within:border-primary"
+					>
+						<span class="text-sm text-muted-foreground">H</span>
+						<input
+							type="number"
+							bind:value={height}
+							class="w-full h-full ml-2 oultine-none text-xs focus:outline-none bg-background"
+						/>
+					</div>
+
+					<div class="p-1.5 h-8 border pointer-events-none text-center">px</div>
+					<Button size="sm">
+						<Check size={20} />
+					</Button>
+				</div>
+
+				<Separator class="my-4" />
+
+				<div class="mb-4 text-sm font-semibold">Presets</div>
+
+				<div class="space-y-4">
+					<div class="flex items-center space-x-2">
+						<Checkbox
+							id="2000x2000"
+							checked={width === 2000 && height === 2000}
+							onCheckedChange={(val) => {
+								if (!val) return;
+								width = 2000;
+								height = 2000;
+							}}
+						/>
+						<Label
+							for="terms"
+							class="text-sm inline-flex font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+						>
+							2000 <X size={16} class="mx-1" /> 2000 px
+						</Label>
+					</div>
+
+					<div class="flex items-center space-x-2">
+						<Checkbox
+							id="1500x1500"
+							checked={width === 1500 && height === 1500}
+							onCheckedChange={(val) => {
+								if (!val) return;
+								width = 1500;
+								height = 1500;
+							}}
+						/>
+						<Label
+							for="terms"
+							class="text-sm inline-flex font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+						>
+							1500 <X size={16} class="mx-1" /> 1500 px
+						</Label>
+					</div>
+
+					<div class="flex items-center space-x-2">
+						<Checkbox
+							id="2000x1500"
+							checked={width === 2000 && height === 1500}
+							onCheckedChange={(val) => {
+								if (!val) return;
+								width = 2000;
+								height = 1500;
+							}}
+						/>
+						<Label
+							for="terms"
+							class="text-sm flex font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+						>
+							2000 <X size={16} class="mx-1" /> 1000 px
+						</Label>
+					</div>
+				</div>
+			</Popover.Content>
+		</Popover.Root>
 
 		<Button variant="secondary">
 			<Folder size={20} class="mr-2" />

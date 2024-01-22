8121268330fb1331f58902c1c55c61f25218e81f
Faadhil Hakim <faadhil@nexttrack.biz>
2024-01-22 10:16:05 -0600

    add layout files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,23 @@
 	import { browser } from '$app/environment';
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import { ModeWatcher } from 'mode-watcher';
-	import { auth } from '$lib/store/auth.svelte';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 
 	const queryClient = new QueryClient({
 		defaultOptions: {
